51b2bbdece2e1abfb6e58ae124fb21793ed7b06d
Pau Lorca <pau.lorca.moreso@gmail.com>
2025-04-25 14:45:49 +0200

    Initial commit (inicial D)

---
+++ android/src/main/java/iticbcn/elhueso/picapollo/android/AndroidLauncher.java
@@ -0,0 +1,18 @@
+package iticbcn.elhueso.picapollo.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import iticbcn.elhueso.picapollo.MainGame;
+
+/** Launches the Android application. */
+public class AndroidLauncher extends AndroidApplication {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration configuration = new AndroidApplicationConfiguration();
+        configuration.useImmersiveMode = true; // Recommended, but not required.
+        initialize(new MainGame(), configuration);
+    }
+}
